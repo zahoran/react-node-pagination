fdac4577eef8e40d2fa2bb295ab3ec58cf1d574d
Daniel Zahoran <daniel.zahoran@gmail.com>
2020-08-17 11:55:22 +0200

    deprecated heroku mongodb url fix

--- mongoose.js
+++ mongoose.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose')
 
-const urlString = process.env.MONGOLAB_URI ||
-    process.env.MONGOHQ_URL ||
+const urlString = process.env.MONGODB_URI ||
     'mongodb://127.0.0.1:27017/mongoose_base'
 
-mongoose.connect(urlString).then(_ => {
+mongoose.connect(urlString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(_ => {
     console.log('mongoose connected')
 }).catch(_ => {
     console.log('mongoose couldn\'t connect')
