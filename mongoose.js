7f27f343fe7ceb8947ac4ae2684bee370300554c
Daniel Zahoran <daniel.zahoran@gmail.com>
2020-08-17 11:43:19 +0200

    fix mongoose

--- mongoose.js
+++ mongoose.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose')
 
+const urlString = process.env.MONGOLAB_URI ||
+    process.env.MONGOHQ_URL ||
+    'mongodb://127.0.0.1:27017/mongoose_base'
+
 mongoose.connect('mongodb://127.0.0.1:27017/mongoose_base', {
     useNewUrlParser: true,
     useCreateIndex: true,
